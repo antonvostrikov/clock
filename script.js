56705002ea00defa647fd213765c4efe0c78da07
antonvostrikov <42900827+antonvostrikov@users.noreply.github.com>
2021-09-30 00:01:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let dateSpan = document.querySelector('.span-date'),
+    timeSpan = document.querySelector('.span-time'),
+    daySpan  = document.querySelector('.span-day');
+
+let date = new Date(),
+    dateDay = date.getDate(),
+    dateMonth  = date.getMonth(),
+    dateHours = date.getHours(),
+    dateMinutes = date.getMinutes(),
+    dateDayOfTheWeek = date.getDay() -1;
+
+    let monthArray = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август',
+     'Сентябрь', 'Октрябрь', 'Ноябрь', 'Декарь'];
+
+    let dayOfTheWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+setInterval(function() {
+    let date = new Date();
+
+    for (let i = 0; i < monthArray.length; i++) {
+        let presentMonth = monthArray[dateMonth];
+        dateSpan.innerHTML = `${dateDay} ${presentMonth}`; 
+    }
+
+    timeSpan.innerHTML = `${dateHours}:${dateMinutes}`;
+    
+    for (let i = 0; i < dayOfTheWeek.length; i++) {
+        let presentDay = dayOfTheWeek[dateDayOfTheWeek];
+        daySpan.innerHTML = presentDay;
+    }
+
+}, 1000);
